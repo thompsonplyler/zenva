97b469ccbf32897affb3e7486662296061159aba
Thompson Plyler <thompson@thompsonplyler.com>
2019-08-01 00:06:02 -0400

    Nearly done with Spanish game.

--- 01 - Road Crossing Game/00 - Starting/js/game.js
+++ 01 - Road Crossing Game/00 - Starting/js/game.js
@@ -1,11 +1,12 @@
 let gameScene = new Phaser.Scene('Game');
 
-gameScene.preload = function(){
+gameScene.preload = function () {
     this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player.png');
     this.load.image('enemy', 'assets/dragon.png');
     this.load.image('treasure', 'assets/treasure.png');
 }
+
 
 let config = {
     type: Phaser.AUTO,
@@ -14,22 +15,23 @@ let config = {
     scene: gameScene
 };
 
-gameScene.init = function (){
-    this.playerSpeed = 2;
-    this.enemyMinSpeed = 1;
-    this.enemyMaxSpeed = 4;
+gameScene.init = function () {
+    this.playerSpeed = 2.5;
+    this.enemyMinSpeed = 2;
+    this.enemyMaxSpeed = 5;
     this.enemyMinY = 80;
     this.enemyMaxY = 280;
-    
+    this.isTerminating = false;
+
 }
 
-gameScene.create = function(){
+gameScene.create = function () {
     let gameW = this.sys.game.config.width
     let gameH = this.sys.game.config.height
-    
-    this.score = 0 
 
-    this.player = this.add.sprite(50,gameH/2, 'player')
+    this.score = 0
+
+    this.player = this.add.sprite(50, gameH / 2, 'player')
     this.player.depth = 1
     // move the player 50 right, 60 down, i.e. away from the origin
     // player.setPosition(100,240)
@@ -39,24 +41,40 @@ gameScene.create = function(){
     // below sets the X and Y to 50% size: 
     this.player.setScale(.5)
 
-    let bg = this.add.sprite(0,0,'background')
-    
+    let bg = this.add.sprite(0, 0, 'background')
 
-    bg.setPosition(gameW/2,gameH/2)
 
-    this.enemy = this.add.sprite(290,gameH/2, 'enemy')
-    let enemy = this.enemy
-    enemy.flipX = true;
-    enemy.scale = .5
-    let dir = Math.random() < 0.5 ? 1 : -1;
-    let speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed)
-    enemy.speed = dir * speed;
+    bg.setPosition(gameW / 2, gameH / 2)
+
+    this.enemies = this.add.group({
+        key: 'enemy',
+        repeat: 5,
+        setXY: {
+            x: 110,
+            y: 100,
+            stepX: 82,
+            stepY: 20
+        },
+    });
+    Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.6, -0.6)
+
+    Phaser.Actions.Call(this.enemies.getChildren(), function (enemy) {
+
+        enemy.flipX = true
+        let dir = Math.random() < 0.5 ? 1 : -1;
+        let speed = this.enemyMinSpeed + Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed)
+        enemy.speed = dir * speed
+
+
+
+
+    }, this)
 
     // enemy.flipX = enemy.setScale(-1,1)
     // enemy.flipY = enemy.setScale(1,-1)
     // to expand the WIDTH but not the HEIGHT of the sprite, 
     // enemy.displayWidth = 300 
-    
+
     // to rotate the enemy 45 degrees either
     // enemy.angle = 45 
     // or 
@@ -66,46 +84,74 @@ gameScene.create = function(){
     // or 
     // enemy.setRotation(Math.PI / 4) (for radians)
 
-    this.treasure = this.add.sprite(gameW-80,gameH/2, 'treasure')
+    this.treasure = this.add.sprite(gameW - 80, gameH / 2, 'treasure')
     this.treasure.scale = .5
-    
+
 }
 
-gameScene.update = function(){
+gameScene.update = function () {
     let player = this.player
     let treasure = this.treasure
     let playerRect = player.getBounds();
     let treasureRect = treasure.getBounds();
-    let score = this.score
-    let enemy = this.enemy
-    // this doesn't work because it becomes a new reserved place in memory. The speed doesn't actually change below.
-    // it must be: this.enemySpeed
-    let enemySpeed = this.enemy.speed
     let enemyMaxY = this.enemyMaxY
     let enemyMinY = this.enemyMinY
-    
+    let enemies = this.enemies.getChildren();
+    let numEnemies = enemies.length;
 
- if(this.input.activePointer.isDown) {
-     player.x += this.playerSpeed
- }
+    if (this.isTerminating) {
+        return
+    }
 
- enemy.y += enemySpeed
+    for (let i = 0; i < numEnemies; i++) {
+        let enemyRect = enemies[i].getBounds();
+        enemies[i].y += enemies[i].speed
+        let conditionDown = enemies[i].speed > 0 && enemies[i].y >= enemyMaxY
+        let conditionUp = enemies[i].speed < 0 && enemies[i].y <= enemyMinY
 
- let conditionDown = enemySpeed > 0 && enemy.y >= enemyMaxY
- let conditionUp = enemySpeed < 0 && enemy.y <= enemyMinY
- 
- if (conditionDown || conditionUp) {
-    this.enemy.speed *= -1
+        if (conditionDown || conditionUp) {
+            enemies[i].speed *= -1
+        }
+
+        if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
+            console.log(`You got touched by an enemy! You lose!`)
+            this.gameOver();
+
+        }
+    }
+
+    // for (let i = 0; i < numEnemies; i++) {
+    //     enemies[i].y += this.enemySpeed
+    // }
+
+
+    if (this.input.activePointer.isDown) {
+        player.x += this.playerSpeed
+    }
+
+
+
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+        score = +1
+        console.log(`You got the treasure! Your score is ${score}`)
+        this.gameOver()
+    }
+
+
 }
 
- if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect,treasureRect)) {
-     score =+ 1
-     console.log(`You got the treasure! Your score is ${score}`)
-     this.scene.restart();   
-     return
- }
-
-
+gameScene.gameOver = function () {
+    this.isTerminating = true
+    this.cameras.main.shake(100);
+    this.cameras.main.on('camerashakecomplete', function () {
+        this.cameras.main.fade(250);
+        this.cameras.main.on('camerafadeoutcomplete', function () {
+            this.scene.restart()
+        }, this)
+    }, this)
+    return
 }
+
 
 let game = new Phaser.Game(config);
---
+++ 02 - Spanish Teaching Game/js/game.js
@@ -0,0 +1,239 @@
+let gameScene = new Phaser.Scene('Game');
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene,
+    title: 'Spanish Learning Game',
+    pixelArt: false
+};
+
+
+gameScene.preload = function () {
+    this.load.image('background', 'assets/images/background-city.png')
+    this.load.image('building', 'assets/images/building.png')
+    this.load.image('car', 'assets/images/car.png')
+    this.load.image('house', 'assets/images/house.png')
+    this.load.image('tree', 'assets/images/tree.png')
+    this.load.audio('treeAudio', 'assets/audio/arbol.mp3')
+    this.load.audio('carAudio', 'assets/audio/auto.mp3')
+    this.load.audio('houseAudio', 'assets/audio/casa.mp3')
+    this.load.audio('buildingAudio', 'assets/audio/edificio.mp3')
+    this.load.audio('correct', 'assets/audio/correct.mp3')
+    this.load.audio('wrong', 'assets/audio/wrong.mp3')
+
+}
+
+
+
+gameScene.init = function () {
+    // word database
+    this.words = [{
+            key: 'building',
+            setXY: {
+                x: 100,
+                y: 220
+            },
+            spanish: 'edificio'
+        },
+
+        {
+            key: 'house',
+            setXY: {
+                x: 500,
+                y: 255
+            },
+            setScale: {
+                x: 0.8,
+                y: 0.8
+            },
+            spanish: 'casa'
+        },
+        {
+            key: 'car',
+            setXY: {
+                x: 250,
+                y: 300
+            },
+            spanish: 'automóvil'
+        },
+        {
+            key: 'tree',
+            setXY: {
+                x: 350,
+                y: 230,
+            },
+            spanish: 'árbol'
+        }
+    ];
+
+}
+
+gameScene.showNextQuestion = function () {
+    let nextWord = Phaser.Math.RND.pick(this.words)
+
+
+}
+
+gameScene.create = function () {
+    let gameW = this.sys.game.config.width
+    let gameH = this.sys.game.config.height
+
+    let soundSample = this.sound.add('correct')
+
+
+    let bg = this.add.sprite(gameW / 2, gameH / 2, 'background')
+    bg.setDepth(0)
+    // we can add an event listener with
+    // bg.setInteractive();
+
+    bg.on('pointerdown', function (pointer) {
+        console.log('click')
+        console.log(pointer)
+        // soundSample.play()
+    })
+
+    // below is how you add individual elements 
+    // video from Zenva showed how to add them to a group/array
+
+    // let building = this.add.sprite(100, 220, 'building')
+    // let house = this.add.sprite(500, 240, 'house')
+    // let car = this.add.sprite(250, 300, 'car')
+    // let tree = this.add.sprite(350, 230, 'tree')
+
+    // this is how to add a group of sprites with different keys.
+    // each item in the group can be acted on with Phaser.Actions.Call, or acted on with conditional statements looking for the key.
+
+    this.items = this.add.group([{
+            key: 'building',
+            setXY: {
+                x: 100,
+                y: 220
+            },
+            index: 0,
+            name: 'building'
+        },
+        {
+            key: 'house',
+            setXY: {
+                x: 500,
+                y: 255
+            },
+            setScale: {
+                x: 0.8,
+                y: 0.8
+            }
+        },
+        {
+            key: 'car',
+            setXY: {
+                x: 250,
+                y: 300
+            }
+        },
+        {
+            key: 'tree',
+            setXY: {
+                x: 350,
+                y: 230,
+            }
+        }
+    ]);
+
+    this.items.setDepth(1)
+
+    Phaser.Actions.Call(this.items.getChildren(), function (item) {
+        let wordArr = item.scene.words
+
+        for (let i = 0; i < wordArr.length; i++) {
+            if (wordArr[i].key === item.texture.key) {
+                this.words[i].sound = this.sound.add(`${item.texture.key}Audio`)
+
+            }
+        }
+
+        // without item.setInteractive, the items can't respond to clicks
+        item.setInteractive();
+
+        item.resizeTween = this.tweens.add({
+            targets: item,
+            scaleX: 1.5,
+            scaleY: 1.5,
+            duration: 300,
+            paused: true,
+            yoyo: true
+        })
+
+        item.alphaTween = this.tweens.add({
+            targets: item,
+            alpha: 0.7,
+            duration: 200,
+            paused: true,
+            ease: 'Circular.InOut'
+        })
+
+        item.on('pointerover', function (pointer) {
+                // console.log(item.alphaTween.data[0])
+                item.alphaTween.resume();
+            }
+
+        )
+
+        item.on('pointerout', function (pointer) {
+            // if we don't stop the tween, it will wait to finish the tween if we leave the mouse very quickly.
+            item.alphaTween.stop();
+            item.alpha = 1;
+        })
+
+
+        item.on('pointerdown', function (pointer) {
+            if (item.resizeTween.isPlaying()) {
+                item.resizeTween.pause();
+                console.log("Halting tween!")
+            } else {
+                item.resizeTween.resume();
+                for (let i = 0; i < wordArr.length; i++) {
+                    if (wordArr[i].key === item.texture.key) {
+                        console.log(wordArr[i].spanish)
+
+                        // this lets you hear the sound of the Spanish on a mouse click.
+                        // this.sound.add(`${item.texture.key}Audio`).play()
+                    }
+                }
+            }
+            // in order to return the name of the clicked object for enumerated items called 'item'
+            // use item.texture.key
+            // console.log(`You clicked on the ${item.texture.key}`)
+            // console.log(pointer)
+            // soundSample.play()
+
+            // this makes the tween above start
+
+            this.showNextQuestion();
+
+        }, this)
+
+    }, this)
+
+    this.showNextQuestion();
+
+
+}
+
+gameScene.update = function () {
+
+    if (this.input.activePointer.isDown) {
+        // this.soundSample.play();
+    }
+
+
+
+}
+
+gameScene.gameOver = function () {
+
+}
+
+
+let game = new Phaser.Game(config);
